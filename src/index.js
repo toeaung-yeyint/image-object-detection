0a5953eba03f6ff3cc7ba936cb0827acdcb7db26
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-05-31 02:04:44 +1000

    added image object detection functionality

--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
+// select form element
 const form = document.querySelector(".form");
-const resultImage = document.querySelector(".resultImage");
-const numberOfDetection = document.querySelector(".numberOfDetection");
-const resultList = document.querySelector(".resultObjectList");
+//  select reset button
+const resetButton = document.querySelector(".reset-button");
+// select result section element
+const result = document.querySelector(".result");
+// event listener to handle the event when a user clicks on detect button
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	numberOfDetection.innerText = "";
-	resultList.innerText = "";
-	const image = document.querySelector("#fileUpload");
-	resultImage.src = URL.createObjectURL(image.files[0]);
-	resultImage.style.width = "400px";
-	resultImage.style.height = "400px";
-	resultImage.style.objectFit = "cover";
+	// remove any child element under result section element
+	result.childNodes.forEach((element) => {
+		element?.remove();
+	});
+	const image = document.querySelector("#image-upload");
+	// create img element
+	const resultImage = document.createElement("img");
 	// create an object detector using cocossd model
 	const objectDetector = ml5.objectDetector("cocossd");
 	// detect objects from image using object detector
 	objectDetector.detect(resultImage).then((objects) => {
-		objects.forEach((object) => {
-			console.log(object.label);
-			console.log(object.confidence);
-			resultList.insertAdjacentHTML(
-				"beforeend",
-				`<li>${object.label} | confidence score: ${Math.round(
-					object.confidence * 100,
-					2
-				)} %</li>`
-			);
-		});
-		numberOfDetection.innerText = `Number of detected object(s): ${objects.length}`;
+		console.log(objects);
 	});
+	// insert result image under result element
+	result.append(resultImage);
+	// convert image object into string for src attribute
+	resultImage.src = URL.createObjectURL(image.files[0]);
+	resultImage.classList.add("result-image");
+	// remove the value from the image input field
 	image.value = "";
+});
+
+// event listener to handle the event when a user clicks on reset button
+resetButton.addEventListener("click", () => {
+	// select image input field
+	const image = document.querySelector("#image-upload");
+	// remove the value from the input field
+	image.value = "";
+	// select result image
+	const resultImage = document.querySelector(".result-image");
+	// remove the image
+	resultImage.remove();
 });
