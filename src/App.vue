fdd46ecb8fca0a2c7d8134cad63028aeaf81313d
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-06-27 04:19:33 +1000

    rewrite the app using vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="max-w-screen-lg my-28 mx-auto">
-    <h1 class="uppercase text-center mb-10">image-object-detection</h1>
-    <p class="">
-      <strong>Built using:</strong> Vue, Composition API, ml5.js, HTML,
-      Tailwind, Vite
-    </p>
-    <p class="mb-16">
-      This app detects specific objects (e.g. a person from an image with a
-      person in it) and display the detected objects along with their respective
-      confidence scores. However, it's important to note that the detection
-      system is not perfect and may not achieve 100% accuracy.
-    </p>
-    <Upload />
-  </div>
+	<div class="max-w-screen-lg my-28 mx-auto">
+		<h1 class="uppercase text-center mb-10 text-3xl font-bold">
+			image-object-detection
+		</h1>
+		<p class="mb-4">
+			<strong>Built using:</strong> Vue, Composition API, ml5.js, HTML,
+			Tailwind, Vite
+		</p>
+		<p class="mb-20">
+			This app detects specific objects (e.g. a person from an image with a
+			person in it) and display the detected objects along with their respective
+			confidence scores. However, it's important to note that the detection
+			system is not perfect and may not achieve 100% accuracy.
+		</p>
+		<DetectForm />
+	</div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import Upload from "./components/Upload.vue";
+import DetectForm from "./components/DetectForm.vue";
 </script>
