4865147222cf41574feb83ff6ba3d720f22b118f
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-06-27 22:31:50 +1000

    added rectangular boxes around the detected objects

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,55 @@
 <template>
-  <div class="max-w-screen-lg my-28 mx-auto px-8">
-    <h1 class="uppercase text-center mb-12 text-3xl font-bold">
-      image-object-detection
-    </h1>
-    <p class="mb-5">
-      <strong>Built using:</strong> Vue, Composition API, ml5.js, HTML, Tailwind
-      CSS, Vite
-    </p>
-    <p class="mb-16">
-      This fully-fledged Vue app utilizes the power of ml5.js, a machine
-      learning JavaScript library, to enable users to identify objects in
-      images. Notably, users have the flexibility to choose from various object
-      detection models such as CocoSsd or YOLO. Additionally, the app shows the
-      detected objects along with their corresponding labels, and confidence
-      scores. Please note that the performance of object detection models relies
-      on the quality of the input image and the inherent limitations of the
-      models(CocoSsd or YOLO) themselves.
-    </p>
-    <DetectForm @detect="handleDetect" @reset="handleReset" />
-    <LoadingBar v-if="loading" />
-    <div v-show="result" class="flex flex-col lg:flex-row items-center">
-      <img
-        class="w-full lg:w-2/3 mb-8 lg:mb-0"
-        :src="src"
-        alt=""
-        ref="resultImage"
-      />
-      <div class="w-full lg:w-1/3 mx-auto grid place-items-center">
-        <div>
-          <h2 class="mb-2 lg:mb-4 text-lg sm:text-xl">
-            Numbers of detected objects: {{ numberOfDetectedObjects }}
-          </h2>
-          <ul class="text-sm">
-            <li v-for="(object, index) in detectedObjects" :key="index">
-              Label: {{ object.label }}, Confience score:
-              {{ Math.round(100 * object.confidence) }}%
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="max-w-screen-lg my-28 mx-auto px-8">
+		<h1 class="uppercase text-center mb-12 text-3xl font-bold">
+			image-object-detection
+		</h1>
+		<p class="mb-5">
+			<strong>Built using:</strong> Vue, Composition API, ml5.js, HTML, Tailwind
+			CSS, Vite
+		</p>
+		<p class="mb-16">
+			This fully-fledged Vue app utilizes the power of ml5.js, a machine
+			learning JavaScript library, to enable users to identify objects in
+			images. Notably, users have the flexibility to choose from various object
+			detection models such as CocoSsd or YOLO. Additionally, the app shows the
+			detected objects along with their corresponding labels, and confidence
+			scores. Please note that the performance of object detection models relies
+			on the quality of the input image and the inherent limitations of the
+			models(CocoSsd or YOLO) themselves.
+		</p>
+		<DetectForm @detect="handleDetect" @reset="handleReset" />
+		<LoadingBar v-if="loading" />
+		<div v-show="result" class="flex flex-col lg:flex-row items-center">
+			<div class="relative w-full lg:w-2/3 mb-8 lg:mb-0" ref="imageWithObjects">
+				<img class="w-full" :src="src" alt="" ref="resultImage" />
+				<span
+					class="hidden cursor-default lg:text-lg lg:font-medium lg:inline-block lg:border-2 lg:border-[#00ff00] lg:text-white lg:absolute lg:bg-black lg:bg-opacity-20 lg:hover:bg-opacity-30"
+					v-for="(object, index) in detectedObjects"
+					:key="index"
+					:style="{
+						top: `${object.y}px`,
+						left: `${object.x}px`,
+						width: `${object.width}px`,
+						height: `${object.height}px`,
+					}"
+					>{{ object.label }}</span
+				>
+			</div>
+			<div class="w-full lg:w-1/3 mx-auto grid place-items-center">
+				<div>
+					<h2 class="mb-2 lg:mb-4 text-lg sm:text-xl">
+						Numbers of detected objects: {{ numberOfDetectedObjects }}
+					</h2>
+					<ul class="text-sm">
+						<li v-for="(object, index) in detectedObjects" :key="index">
+							Label: {{ object.label }}, Confience score:
+							{{ Math.round(100 * object.confidence) }}%
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
@@ -54,27 +63,26 @@ const src = ref(null);
 const resultImage = ref(null);
 const numberOfDetectedObjects = ref(null);
 const detectedObjects = ref([]);
+const imageWithObjects = ref(null);
 
 const handleDetect = async (payload) => {
-  result.value = false;
-  loading.value = true;
-  const inputImage = payload.inputImage.value;
-  const model = payload.model.value;
-  src.value = URL.createObjectURL(inputImage.files[0]);
-
-  // create an object detector using the model cocossd or yolo
-  const objectDetector = await ml5.objectDetector(model);
-  // detect objects from image using object detector,
-  await objectDetector.detect(resultImage.value).then((objects) => {
-    numberOfDetectedObjects.value = objects.length;
-    detectedObjects.value = objects;
-    console.log(detectedObjects);
-    result.value = true;
-    loading.value = false;
-  });
+	result.value = false;
+	loading.value = true;
+	const inputImage = payload.inputImage.value;
+	const model = payload.model.value;
+	src.value = URL.createObjectURL(inputImage.files[0]);
+	// create an object detector using the model cocossd or yolo
+	const objectDetector = await ml5.objectDetector(model);
+	// detect objects from image using object detector,
+	await objectDetector.detect(resultImage.value).then((objects) => {
+		numberOfDetectedObjects.value = objects.length;
+		detectedObjects.value = objects;
+		result.value = true;
+		loading.value = false;
+	});
 };
 
 const handleReset = () => {
-  result.value = false;
+	result.value = false;
 };
 </script>
