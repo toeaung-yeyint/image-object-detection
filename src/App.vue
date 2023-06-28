2ac0159d8b384a3365150ae7e9c98912b6e6e55d
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-06-28 22:50:42 +1000

    improved styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="max-w-screen-lg my-28 mx-auto px-8">
+	<div class="max-w-screen-lg my-28 mx-auto px-4">
 		<h1 class="uppercase text-center mb-12 text-3xl font-bold">
 			image-object-detection
 		</h1>
@@ -19,12 +19,22 @@
 		</p>
 		<DetectForm @detect="handleDetect" @reset="handleReset" />
 		<LoadingBar v-if="loading" />
-		<div v-show="result" class="flex flex-col lg:flex-row items-center">
-			<div
-				class="relative w-full lg:w-2/3 mb-8 lg:mb-0"
-				ref="resultImageContainer"
-			>
+		<div v-show="result" class="flex flex-col lg:flex-row">
+			<div class="relative mb-8 lg:mb-0 w-full lg:w-2/3">
 				<img class="w-full" :src="src" alt="" ref="resultImage" />
+				<span
+					class="text-[#00ff00] inline-block cursor-default text-xs sm:text-base lg:font-medium border-2 border-[#00ff00] absolute lg:bg-black lg:bg-opacity-20 lg:hover:bg-opacity-30"
+					v-for="(object, index) in detectedObjects"
+					:key="index"
+					:style="{
+						top: `${object.y * scalingFactor}px`,
+						left: `${object.x * scalingFactor}px`,
+						width: `${object.width * scalingFactor}px`,
+						height: `${object.height * scalingFactor}px`,
+					}"
+				>
+					{{ index + 1 }}
+				</span>
 			</div>
 			<div class="w-full lg:w-1/3 mx-auto grid place-items-center">
 				<div>
@@ -54,6 +64,17 @@ const numberOfDetectedObjects = ref(null);
 const detectedObjects = ref([]);
 const src = ref(null);
 const resultImage = ref(null);
+const resultImageContainer = ref(null);
+const scalingFactor = ref(null);
+
+onMounted(() => {
+	window.addEventListener("resize", () => {
+		if (result.value) {
+			scalingFactor.value =
+				resultImage.value.offsetWidth / resultImage.value.naturalWidth;
+		}
+	});
+});
 
 const handleDetect = async (payload) => {
 	result.value = false;
@@ -67,9 +88,12 @@ const handleDetect = async (payload) => {
 	await objectDetector.detect(resultImage.value).then((objects) => {
 		numberOfDetectedObjects.value = objects.length;
 		detectedObjects.value = objects;
+
 		result.value = true;
 		loading.value = false;
 	});
+	scalingFactor.value =
+		resultImage.value.offsetWidth / resultImage.value.naturalWidth;
 };
 
 const handleReset = () => {
